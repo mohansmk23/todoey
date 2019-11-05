1bc0cbc0980a9f6302a1b002115ac86ba894bcda
MohanKumar S <mohankumar.istrides@gmail.com>
2019-11-05 19:25:26 +0530

    Adding state to checkbox

--- lib/widgets/task_tiles.dart
+++ lib/widgets/task_tiles.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TaskTiles extends StatelessWidget {
-  const TaskTiles({
-    Key key,
-  }) : super(key: key);
+class TaskTiles extends StatefulWidget {
+  @override
+  _TaskTilesState createState() => _TaskTilesState();
+}
+
+class _TaskTilesState extends State<TaskTiles> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("todo 1"),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
+      title: Text(
+        "todo 1",
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: new TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
       ),
     );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        checkColor: Colors.lightBlueAccent,
+        value: checkboxState,
+        onChanged: toggleCheckboxState);
   }
 }
